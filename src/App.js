8ce2b69e758513673abc6117c59f02932d721a84
Thomas Woodward <tp.woodward@hotmail.com>
2020-09-24 22:21:19 +0800

    Contact components included

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "./scss/master.scss"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-
 import Header from "./components/Header"
 import Articles from "./components/Articles"
 import Health from "./components/Health"
 import Help from "./components/Help"
 import Footer from "./components/Footer"
 
+import FAQ from "./components/FAQ"
+import Contact from "./components/Contact"
 import Article1 from "./components/Article1"
 import Article2 from "./components/Article2"
 import HealthCalc from "./components/HealthCalc"
-
 
 function App() {
   return (
@@ -22,6 +22,12 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
         <Switch>
+          <Route path="/faq">
+            <FAQ />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
           <Route path="/hiit-vs-liss">
             <Article1 />
           </Route>
--- src/components/Help.js
+++ src/components/Help.js
@@ -1,10 +1,30 @@
 import React from "react"
 
+import { FaArrowLeft } from "react-icons/fa"
+import { FaArrowRight } from "react-icons/fa"
+
+
 function Help() {
   return (
     <div id="help">
       <div className="Inner">
-        <h1 className="display-1">Help</h1>
+      <div className="container mw-100">
+            <div className="row">
+              <div className="col subsectionbtn text-left">
+                <a href="/faq" className="arrow text-white">
+                  <FaArrowLeft />
+                </a>
+                <p>FAQ</p>
+              </div>
+              <div className="col subsectionbtn text-right">
+                <a href="/contact" className="arrow text-white">
+                  <FaArrowRight />
+                </a>
+                <p>Contact</p>
+              </div>
+            </div>
+          </div>
+          <h1 className="display-1 text-right">Help</h1>
       </div>
     </div>
   )
