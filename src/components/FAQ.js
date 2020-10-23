741dd0a01d1d3b4ba8b7c12f7e4c9a18ba547059
Thomas Woodward <tp.woodward@hotmai.com>
2020-10-23 17:49:45 +0800

    added faq content

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,28 +1,55 @@
 import React from "react"
-import { FaArrowRight} from "react-icons/fa"
+import { FaArrowRight } from "react-icons/fa"
+import Nav from "./Nav"
 
 function FAQ() {
   return (
     <div className="Help">
+      <Nav />,
       <div className="Inner">
+        <h1 className="display-2">Frequently Asked Questions</h1>
+        <a href="/" className="arrow text-white">
+          <FaArrowRight />
+        </a>
         <div className="container mw-100">
           <div className="row">
             <div className="col">
-              <h1 className="display-2">FAQ</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
               </p>
             </div>
-            <div className="col-2 subsectionbtn text-right">
-              <a href="/" className="arrow text-white">
-                <FaArrowRight />
-              </a>
+            <div className="col">
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
+              </p>
+            </div>
+            <div className="col">
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
+              </p>
+            </div>
+            <div className="col">
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
+              </p>
+            </div>
+            <div className="col">
+              <p className="question">Question</p>
+              <p className="answer">
+                Answer
+              </p>
             </div>
           </div>
         </div>
