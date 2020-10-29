7763d8b4bc0212be6fb330ccccc43aaa9ea98257
Thomas Woodward <tp.woodward@hotmai.com>
2020-10-29 21:54:14 +0800

    article content

--- src/components/Article1.js
+++ src/components/Article1.js
@@ -6,42 +6,45 @@ function Article1() {
     <div className="Article">
       <SubNav />
       <div className="Inner">
-        <div className="container mw-100">
-          <div className="row">
-            <div className="col">
-              <h2 className="display-3">HIIT vs LISS</h2>
+              <h2>HIIT vs LISS</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                With so many different types of exercise out there to choose
+                from, it can be difficult to know which one is best for you and
+                your goals. If you’re trying to build muscle and burn fat, you
+                may be left wondering if you should be doing Low-Intensity
+                Steady State (LISS) cardio or High-Intensity Interval Training
+                (HIIT) as a supplement to your strength training. In order to
+                answer this, you need to understand the differences between LISS
+                cardio and HIIT.
               </p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                So what are the differences between HIIT and LISS Cardio? HIIT,
+                or High-Intensity Interval Training, is short in duration
+                (5-20-minutes) and higher in intensity. LISS, or Low-Intensity
+                Steady-State, is longer in duration (30-60 minutes) and lower in
+                intensity. HIIT is also more efficient at burning calories than
+                LISS.
               </p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                While both HIIT and LISS have their own benefits, the
+                differences between the two can help you understand which is
+                better for your training goals. If your goal is fat loss, HIIT
+                will be more effective. This is because HIIT is higher in
+                intensity and therefore requires less training time, less
+                training frequency and still burns more calories. If your goal
+                is to get stronger, HIIT will be a better option as there is
+                less risk of burning muscle mass. This isn’t to say that LISS
+                shouldn’t be included in your training, as it provides benefits
+                for heart health, endurance and the lower intensity nature of
+                LISS workouts means your body can recover quickly.
+              </p>
+              <p className="reference">
+                Morgan, Maggie. 2020. "3 Differences Between LISS Cardio And
+                HIIT (#3 Is Cool)." Fitbod.
+                https://tinyurl.com/yxudf239.
               </p>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
   )
 }
 
--- src/components/Article2.js
+++ src/components/Article2.js
@@ -1,48 +1,54 @@
 import React from "react"
 import SubNav from "./SubNav"
 
-function Article1() {
+function Article2() {
   return (
     <div className="Article">
       <SubNav />
       <div className="Inner">
-        <div className="container mw-100">
-          <div className="row">
-            <div className="col">
-              <h2 className="display-3">What are macros?</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-          </div>
-        </div>
+        <h2>What are macros?</h2>
+        <p>
+          Macronutrients, also known as macros, are the three essential
+          nutritional components that make up the caloric content of the foods
+          we consume. These essential nutritional components are also known as
+          fats, proteins and carbohydrates. Simply put, our body uses these
+          components as a fuel source to give us the energy required to go about
+          our daily tasks - from work and chores to exercise… but that’s not all
+          they have to offer!
+        </p>
+        <p>
+          As the name suggests, macronutrients are nutrients, and nutrients are
+          substances required for growth, tissue repair, energy provision, body
+          temperature regulation, workout fuel, and various other functions.
+          Energy comes from foods rich in carbohydrates, protein and fats,
+          however, the amount of energy provided depends on the type of
+          macronutrient. For example - According to Healthline, both
+          carbohydrates and proteins contain 4 calories per 1g, whereas fats
+          contain 9 calories per gram.
+        </p>
+        <p>
+          Your ideal macro ratio will depend on your goals and unique
+          requirements, which is why it’s important to seek assistance from a
+          qualified professional, including a Dietitian or Personal Trainer, to
+          find the right breakdown for you. Likewise, goal-based macro ratio
+          breakdowns may vary from source to source.
+        </p>
+        <p>
+          Before the days of technology, tracking macros required a lot of
+          manual work. Now, tracking your macros is as easy as clicking a
+          button. With apps such as MyFitnessPal and MyMacros+ doing the
+          calculations for you, all you need to do is scan the barcode on the
+          package, weigh the amount you’ve had, and all the calculations are
+          done for you.
+        </p>
+        <p className="reference">
+          Australian Sports Nutrition. 2019. "Macros 101: What are they and why
+          do I need them?" Australian Sports Nutrition.
+          https://tinyurl.com/y3bxulgn.
+        </p>
       </div>
     </div>
   )
 }
 
-export default Article1
+export default Article2
