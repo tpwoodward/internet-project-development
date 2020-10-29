0a17da49d5f0565052c025d000083990b4fc88fb
Thomas Woodward <tp.woodward@hotmai.com>
2020-10-30 00:04:12 +0800

    background images added

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,30 +1,31 @@
 import React from "react"
-import Nav from "./Nav"
-
-import { Parallax }  from "react-scroll-parallax"
+import { Parallax } from "react-scroll-parallax"
 
 import { BiRun, BiFoodMenu } from "react-icons/bi"
+import { VscBeaker } from "react-icons/vsc"
+
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function Articles() {
   return (
-    <div>
-      <Nav />
-      <div id="articles">
-        <div className="Inner">
-        <Parallax className="scrolling-section" y={[-100, 25]}>
-          <h1 className="display-1">Articles</h1>
-          </Parallax>
-          <a href="/hiit-vs-liss" className="button mainBtn">
-            <BiRun className="btnIcon" />
-            HIIT vs LISS
-          </a>
-          <a href="/what-are-macros" className="button mainBtn">
-            <BiFoodMenu className="btnIcon" />
-            What are macros?
-          </a>
-        </div>
-      </div>
-    </div>
+
+          <div id="articles">
+            <div className="inner">
+              <Parallax className="scrolling-section" y={[-100, 25]}>
+                <h1 className="display-1">Articles</h1>
+              </Parallax>
+              <a href="/hiit-vs-liss" className="button mainBtn">
+                <BiRun className="btnIcon" />
+                HIIT vs LISS
+              </a>
+              <a href="/what-are-macros" className="button mainBtn">
+                <BiFoodMenu className="btnIcon" />
+                What are macros?
+              </a>
+            </div>
+          </div>
   )
 }
 
--- src/components/Health.js
+++ src/components/Health.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Parallax }  from "react-scroll-parallax"
+import { Parallax } from "react-scroll-parallax"
 
 import { BiBody, BiPieChartAlt2, BiBarChartAlt } from "react-icons/bi"
 import Button from "react-bootstrap/Button"
@@ -16,33 +16,30 @@ function Health() {
   return (
     <div id="your-health">
       <div className="Inner">
-      <Parallax className="scrolling-section" y={[-100, 25]}>
-      <h1 className="display-1">Your Health</h1>
-      </Parallax>
-              <Button onClick={() => setBmiModalShow(true)} bsPrefix="button mainBtn">
-                <BiBody className="btnIcon"/>
-                Body Mass Index
-              </Button>
-              <BMIModal
-                show={bmiModalShow}
-                onHide={() => setBmiModalShow(false)}
-              />
-              <Button onClick={() => setBodyFatModalShow(true)} bsPrefix="button">
-              <BiPieChartAlt2 className="btnIcon"/>
-                Body Fat Percentage
-              </Button>
-              <BodyFatModal
-                show={bodyFatModalShow}
-                onHide={() => setBodyFatModalShow(false)}
-              />
-              <Button onClick={() => setCaloriesModalShow(true)} bsPrefix="button">
-              <BiBarChartAlt className="btnIcon"/>
-                Calorie Requirements
-              </Button>
-              <CaloriesModal
-                show={caloriesModalShow}
-                onHide={() => setCaloriesModalShow(false)}
-              />
+        <Parallax className="scrolling-section" y={[-100, 25]}>
+          <h1 className="display-1">Your Health</h1>
+        </Parallax>
+        <Button onClick={() => setBmiModalShow(true)} bsPrefix="button mainBtn">
+          <BiBody className="btnIcon" />
+          Body Mass Index
+        </Button>
+        <BMIModal show={bmiModalShow} onHide={() => setBmiModalShow(false)} />
+        <Button onClick={() => setBodyFatModalShow(true)} bsPrefix="button">
+          <BiPieChartAlt2 className="btnIcon" />
+          Body Fat Percentage
+        </Button>
+        <BodyFatModal
+          show={bodyFatModalShow}
+          onHide={() => setBodyFatModalShow(false)}
+        />
+        <Button onClick={() => setCaloriesModalShow(true)} bsPrefix="button">
+          <BiBarChartAlt className="btnIcon" />
+          Calorie Requirements
+        </Button>
+        <CaloriesModal
+          show={caloriesModalShow}
+          onHide={() => setCaloriesModalShow(false)}
+        />
       </div>
     </div>
   )
